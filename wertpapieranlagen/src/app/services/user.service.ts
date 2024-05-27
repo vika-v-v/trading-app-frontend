22454c0f54b664d0620fd7cce13e7540c11c555d
kampmeierj <joshuakampmeier@gmail.com>
2024-05-27 18:24:37 +0200

    [updated] user.service.ts

--- wertpapieranlagen/src/app/services/user.service.ts
+++ wertpapieranlagen/src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { HttpParams } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +9,17 @@ import { Observable } from 'rxjs';
 export class UserService {
   private rootUrl: string;
 
+  private token!: string;
+
   constructor(@Inject('ROOT_URL') rootUrl: string) {
     this.rootUrl = rootUrl;
+  }
+
+  setToken(token: string) {
+    this.token = token;
+  }
+  getToken(){
+    return this.token;
   }
 
   login(http: HttpClient, email: string, passwort: string): Observable<any> {
