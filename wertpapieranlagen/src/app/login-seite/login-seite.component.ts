8abf2ee75f69cc877e720159df425f6558aba6df
vika-v <vika.vovchenkoo@gmail.com>
2024-06-03 12:05:41 +0200

    [changed] background-image

--- wertpapieranlagen/src/app/login-seite/login-seite.component.ts
+++ wertpapieranlagen/src/app/login-seite/login-seite.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../services/user.service';
 import { FormsModule } from '@angular/forms';
