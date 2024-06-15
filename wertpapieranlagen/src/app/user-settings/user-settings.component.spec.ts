e825ff89a1af8a86d26847e2f7f3c41e755547d8
kampmeierj <joshuakampmeier@gmail.com>
2024-06-15 18:20:56 +0200

    user-settings.component.spec.ts

--- wertpapieranlagen/src/app/user-settings/user-settings.component.spec.ts
+++ wertpapieranlagen/src/app/user-settings/user-settings.component.spec.ts
@@ -1,6 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UserSettingsComponent } from './user-settings.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { UserService } from '../services/user.service';
+import { AutoLogoutService } from '../services/auto-logout.service';
+import { Component } from '@angular/core';
+
+// Mock components for testing
+@Component({
+  selector: 'app-tax-settings',
+  template: ''
+})
+class MockTaxSettingsComponent {}
+
+@Component({
+  selector: 'app-change-password-page',
+  template: ''
+})
+class MockChangePasswordPageComponent {}
 
 describe('UserSettingsComponent', () => {
   let component: UserSettingsComponent;
@@ -8,10 +24,23 @@ describe('UserSettingsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UserSettingsComponent]
+      declarations: [
+        MockTaxSettingsComponent, // Mock of TaxSettingsComponent
+        MockChangePasswordPageComponent // Mock of ChangePasswordPageComponent
+      ],
+      imports: [
+        RouterTestingModule // For mocking the Router
+      ],
+      providers: [
+        UserService,
+        AutoLogoutService,
+        { provide: 'ROOT_URL', useValue: 'http://example.com/api' } // Example of providing ROOT_URL
+      ]
     })
-    .compileComponents();
-    
+      .compileComponents();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(UserSettingsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +48,27 @@ describe('UserSettingsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show steuerrechner when showSteuerrechner is called', () => {
+    component.showSteuerrechner();
+    expect(component.steuerrechnerShown).toBe(true);
+    expect(component.changePasswordShown).toBe(false);
+  });
+
+  it('should hide steuerrechner when hideSteuerrechner is called', () => {
+    component.hideSteuerrechner();
+    expect(component.steuerrechnerShown).toBe(false);
+  });
+
+  it('should show changePassword when showChangePassword is called', () => {
+    component.showChangePassword();
+    expect(component.changePasswordShown).toBe(true);
+    expect(component.steuerrechnerShown).toBe(false);
+  });
+
+  it('should hide changePassword when hideChangePassword is called', () => {
+    component.hideChangePassword();
+    expect(component.changePasswordShown).toBe(false);
   });
 });
