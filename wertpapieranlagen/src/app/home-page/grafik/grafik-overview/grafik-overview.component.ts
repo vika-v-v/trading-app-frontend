6315ee42dcc5fed06e05d2cec103a8a83e342fc7
vika-v <vika.vovchenkoo@gmail.com>
2024-06-18 21:07:31 +0200

    working on graphics

--- wertpapieranlagen/src/app/home-page/grafik/grafik-overview/grafik-overview.component.ts
+++ wertpapieranlagen/src/app/home-page/grafik/grafik-overview/grafik-overview.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { GrafikLinechartDepotComponent } from '../grafik-linechart-depot/grafik-linechart-depot.component';
 import { GrafikPiechartNumberComponent } from '../grafik-piechart-number/grafik-piechart-number.component';
 import { GrafikPiechartValueComponent } from '../grafik-piechart-value/grafik-piechart-value.component';
 import { CommonModule } from '@angular/common';
+import { GrafikTyp } from '../grafik-typ.enum';
 
 @Component({
   selector: 'app-grafik-overview',
@@ -12,7 +13,10 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule, GrafikLinechartDepotComponent, GrafikPiechartNumberComponent, GrafikPiechartValueComponent]
 })
 export class GrafikOverviewComponent implements OnInit {
+  grafikTyp = GrafikTyp;
   selectedDepotName: string | null = null;
+
+  @Input() typ = GrafikTyp.WertverlaufDepotwerte;
 
   constructor() { }
 
