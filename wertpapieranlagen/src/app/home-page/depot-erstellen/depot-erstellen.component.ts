dbbd561d04ee4ad0a3f6ee8722fa94d449c3fea9
vika-v <vika.vovchenkoo@gmail.com>
2024-06-19 10:56:30 +0200

    [added] styles to depot erstellen

--- wertpapieranlagen/src/app/home-page/depot-erstellen/depot-erstellen.component.ts
+++ wertpapieranlagen/src/app/home-page/depot-erstellen/depot-erstellen.component.ts
@@ -3,24 +3,26 @@ import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { DepotService } from '../../services/depot.service';
 import { FormsModule } from '@angular/forms';
 import { DepotDropdownService } from '../../services/depot-dropdown.service';
+import { CustomDropdownComponent } from '../../custom-dropdown/custom-dropdown.component';
 
 
 @Component({
   selector: 'app-depot-erstellen',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CustomDropdownComponent],
   templateUrl: './depot-erstellen.component.html',
   styleUrl: './depot-erstellen.component.css'
 })
 export class DepotErstellenComponent implements OnInit{
   currentDate!: string;
   name!: string;
-  waehrung!: string;
+  waehrung: string = 'EUR';
+
+  moeglicheWaehrungen = [{'value': 'EUR', 'label': 'EUR - Euro'}, {'value': 'USD', 'label': 'USD - US-Dollar'}];
 
   @Output() onAbbrechen = new EventEmitter<void>();
 
   constructor(private httpClient: HttpClient, private depotDropdownService: DepotDropdownService, private depotService: DepotService){
-
   }
 
   ngOnInit() {
@@ -31,6 +33,10 @@ export class DepotErstellenComponent implements OnInit{
     this.name = "";
     this.waehrung = "";
     this.onAbbrechen.emit();
+  }
+
+  changeWaehrung(waehrung: string) {
+    this.waehrung = waehrung;
   }
 
   formatDate(date: Date): string {
