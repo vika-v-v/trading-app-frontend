6f0f1a944725232baef64f5e81aedaa194b27b99
vika-v <vika.vovchenkoo@gmail.com>
2024-06-27 20:56:37 +0200

    refactored range-slider

--- wertpapieranlagen/src/app/home-page/tabelle/range-slider/range-slider.component.ts
+++ wertpapieranlagen/src/app/home-page/tabelle/range-slider/range-slider.component.ts
@@ -13,14 +13,18 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './range-slider.component.css'
 })
 export class RangeSliderComponent {
+  // Am Anfrang Werte für die Range-Slider eingeben
   @Input() min!: number;
   @Input() max!: number;
   @Input() step!: number;
   @Input() value1!: number;
   @Input() value2!: number;
+
+  // Andere Komponenten notifizieren, wenn sich die Werte ändern
   @Output() value1Change = new EventEmitter<number>();
   @Output() value2Change = new EventEmitter<number>();
 
+  // Für die Darstellung des Tracks benötigt
   trackLeft: string = '';
   trackWidth: string = '';
 
@@ -28,6 +32,7 @@ export class RangeSliderComponent {
     this.updateTrackStyles();
   }
 
+  // Werte runden und über Änderungen notifizieren
   onValue2Change() {
     this.value2 = parseFloat(this.value2.toFixed(2));
     this.value2Change.emit(this.value2);
@@ -40,6 +45,7 @@ export class RangeSliderComponent {
     this.onValueChange();
   }
 
+  // Werte tauschen, falls value1 > value2
   onValueChange() {
     if (this.value1 > this.value2) {
       [this.value1, this.value2] = [this.value2, this.value1];
